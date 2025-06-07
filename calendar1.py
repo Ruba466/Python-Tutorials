2daa8affb19abb18b7a1ebc6fd0a4233a6467a98
Ruba <rubarubs575@gmail.com>
2025-06-07 18:03:33 +0530

    Added a program that uses Calendar module

---
+++ calendar1.py
@@ -0,0 +1,19 @@
+import calendar
+
+month=int(input("Enter a month (1-12) :"))
+year=int(input("Enter a  year:"))
+print(calendar.month(year,month))
+
+
+'''
+Enter a month (1-12) :10
+Enter a  year:2003
+    October 2003
+Mo Tu We Th Fr Sa Su
+       1  2  3  4  5
+ 6  7  8  9 10 11 12
+13 14 15 16 17 18 19
+20 21 22 23 24 25 26
+27 28 29 30 31
+
+'''
